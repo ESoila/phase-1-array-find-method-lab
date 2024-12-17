2c6432328b2ec3888d7acbaac2e973388302466f
soila <elizabeth.soila@student.moringaschool.com>
2024-12-17 11:11:57 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record) {
+    // Use the find method to locate the first object with result === "W"
+    const win = record.find((game) => game.result === "W");
+    
+    // If a win is found, return the year; otherwise, return undefined
+    return win ? win.year : undefined;
+  }
+  function superbowlWin(record) {
+    const win = record.find((game) => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
